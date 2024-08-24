7423b2f8615ab10c5406b0bd84cb3c6dd9df6e22
thongkhoav <khoaxilo951@gmail.com>
2024-08-24 19:59:37 +0700

    Common update

--- common/src/index.ts
+++ common/src/index.ts
@@ -18,3 +18,4 @@ export * from "./events/base-publisher";
 export * from "./events/subjects";
 export * from "./events/ticket-created-event.type";
 export * from "./events/ticket-updated-event.type";
+export * from "./events/types/order-status";
